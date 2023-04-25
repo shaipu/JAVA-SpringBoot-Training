8dacecb8734e9bf52d0895d2175374d90c88b1a3
Shailesh Puranik <shaileshp@synerzipune.local>
2023-04-26 00:42:02 +0530

    Added MyDate Example

--- MyDateExample/src/Entry.java
+++ MyDateExample/src/Entry.java
@@ -1,7 +1,7 @@
 import com.encora.util.MyDate;
 public class Entry {
 	public static void main(String[] args) {
-		MyDate d = new MyDate(-1,11,2034);
+		MyDate d = new MyDate(-3,11,2031);
 		
 		System.out.println(d);
 	}
--- MyDateExample/src/com/encora/util/MyDate.java
+++ MyDateExample/src/com/encora/util/MyDate.java
@@ -30,11 +30,11 @@ public class MyDate {
 			else if(day<1)
 				this.day=1;
 		
-		if(month==1 || month==3 || month==5 || month==7 || month==8 || month==10 || month==12 && day>=31)
+		if((month==1 || month==3 || month==5 || month==7 || month==8 || month==10 || month==12) && (day>=31))
 			this.day=31;
 		else if(day<1)
 			this.day=1;
-		else if(month==4 || month==6 || month==9 || month==11 && day>=30)
+		else if((month==4 || month==6 || month==9 || month==11) && (day>=30))
 			this.day=30;
 		else if(day<1)
 			this.day=1;
